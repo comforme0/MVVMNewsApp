21d3fa05f37c573862c1ec84c8581bfe775989cd
장경호 <khjang@jang-gyeonghoui-MacBookPro.local>
2020-09-09 23:32:25 +0900

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "728d1e21af904aaa8bfdc16f9a4d319d"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
